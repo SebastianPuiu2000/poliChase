97e93d4b203b4265cfc10d91ce699f753a441117
Puiu-Sebastian-Andrei <scorpionsebi@yahoo.com>
2023-01-04 13:20:14 +0200

    Added interval to send players info to client

--- backend/src/handlers/socket.ts
+++ backend/src/handlers/socket.ts
@@ -2,19 +2,28 @@ import { WebSocketServer } from "ws";
 import { getToken, verify } from "../jwt";
 
 interface Coord {
-  lat: number,
-  lon: number
+  lat: number;
+  lon: number;
 }
 
 interface Player {
-    socket: any,
-    coords: Coord
+  socket: any;
+  coords: Coord;
+  color: string;
+}
+
+interface PlayerInfo {
+  coords: Coord;
+  color: string;
 }
 
 export const listen = (socketPort: number) => {
+  const green = "green";
+  const red = "red";
+
   const wss = new WebSocketServer({ port: socketPort });
 
-  let sockets = new Map <string, Player> ();
+  let sockets = new Map<string, Player>();
 
   wss.on("connection", function connection(socket, req) {
     const token = getToken(req.url);
@@ -31,7 +40,11 @@ export const listen = (socketPort: number) => {
 
     if (sockets.has(payload.id)) return socket.close();
 
-    sockets.set(payload.id, { socket: socket, coords: {lat: 0.0, lon: 0.0} });
+    sockets.set(payload.id, {
+      socket: socket,
+      coords: { lat: 0.0, lon: 0.0 },
+      color: green,
+    });
 
     socket.on("message", async (bytes) => {
       const msg = bytes.toString().split(" ");
@@ -42,11 +55,20 @@ export const listen = (socketPort: number) => {
         sockets.set(payload.id, {
           socket: socket,
           coords: { lat: parseFloat(msg[1]), lon: parseFloat(msg[2]) },
+          color: green,
         });
     });
 
+    const interval = setInterval(() => {
+      let players: PlayerInfo[] = [];
+      for (let value of sockets.values())
+        players.push({ coords: value.coords, color: value.color });
+      socket.send(players);
+    }, 1000);
+
     socket.on("close", async (_) => {
       sockets.delete(payload.id);
+      clearInterval(interval);
     });
   });
 };
